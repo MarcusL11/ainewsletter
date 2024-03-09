d5340566fc5ea9dc113ec80bc76ce4049e20959e
MarcusL11 <marclert@gmail.com>
2024-03-09 12:32:19 +0700

    adding htmx to the project and adding search blog function

--- newsletterproject/blogapp/urls.py
+++ newsletterproject/blogapp/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path("blog/<slug:slug>/", views.blog, name='blog'),
+    path("", views.home, name="home"),
+    path("blog/<slug:slug>/", views.blog, name="blog"),
 ]
+
+
+htmx_urlpatterns = {
+    path("search/", views.search_trigger, name="search-trigger"),    
+    path("search/close/", views.search_trigger_close, name="search-trigger-close"),
+    path("check_post/", views.check_post, name="check-post"),
+}
+
+urlpatterns += htmx_urlpatterns
--- newsletterproject/blogapp/views.py
+++ newsletterproject/blogapp/views.py
@@ -1,31 +1,58 @@
 from django.shortcuts import render
 from .models import BlogPost
 from django.http import Http404
+from django.http import HttpResponse
 
-def home(request):    
-    blogs = BlogPost.objects.all().order_by('-publish_date')
+
+def home(request):
+    blogs = BlogPost.objects.all().order_by("-publish_date")
     latest_blog = blogs.first()
-    
+
     context = {
-        'blogs': blogs,
-        'latest_blog': latest_blog,
+        "blogs": blogs,
+        "latest_blog": latest_blog,
     }
-    
-    return render(request, 'blogapp/index.html', context)
+
+    return render(request, "blogapp/index.html", context)
+
 
 def blog(request, slug):
     try:
         blog = BlogPost.objects.get(slug=slug)
-        
+
         context = {
-            'blog': blog,
+            "blog": blog,
         }
-    
-    except BlogPost.DoesNotExist:        
+
+    except BlogPost.DoesNotExist:
         return Http404("Blog does not exist.")
-    
-    return render(request, 'blogapp/blog.html', context)
+
+    return render(request, "blogapp/blog.html", context)
 
 
+def search_trigger(request):
+    return render(request, "partials/_search_blog.html")
 
+
+def search_trigger_close(request):
+    return render(request, "partials/_search_button.html")
+
+
+def check_post(request):
+    seach_content = request.POST.get("seach-content", "").strip()
+    if seach_content:
+        matching_title = BlogPost.objects.filter(title__icontains=seach_content)
+        matching_tags = BlogPost.objects.filter(tags__name__icontains=seach_content)
+        matching_content = BlogPost.objects.filter(content__icontains=seach_content)
+
+        context = {
+            "matching_title": matching_title,  
+            "matching_tags": matching_tags,
+            "matching_content": matching_content,
+        }            
+        
+        return render(request, "partials/_search_results.html", context)
+        
+    else: 
+        return HttpResponse("")
 
